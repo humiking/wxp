e1c32cf8586f5efff40b42dbced95a75a729dbc4
humiking <30611192+humiking@users.noreply.github.com>
2017-12-13 23:54:10 +0800

    Add files via upload

--- 小程序/pages/index/index.js
+++ 小程序/pages/index/index.js
@@ -7,24 +7,24 @@ Page({
     motto: 'Hello World',
     userInfo: {},
     hasUserInfo: false,
-    canIUse: wx.canIUse('button.open-type.getUserInfo')
+    canIUse: wx.canIUse('button.open-type.getUserInfo')//判断是否当前按钮在当前版本可用
   },
   //事件处理函数
-  bindViewTap: function() {
-    wx.navigateTo({
+  bindViewTap: function() {//自定义函数事件，绑定转换视图路径事件
+    wx.navigateTo({//跳转应用内非tabBar的页面路径，保留当前页面
       url: '../logs/logs'
     })
   },
-  onLoad: function () {
-    if (app.globalData.userInfo) {
-      this.setData({
+  onLoad: function () {//加载页面时
+    if (app.globalData.userInfo) {//如果存在userInfo的时候，
+      this.setData({//设置page.data.userInfo：app.globalData.userInfo
         userInfo: app.globalData.userInfo,
         hasUserInfo: true
       })
-    } else if (this.data.canIUse){
+    } else if (this.data.canIUse){//如果不存在app.globalData.userInfo,判断按钮是否可用
       // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
       // 所以此处加入 callback 以防止这种情况
-      app.userInfoReadyCallback = res => {
+      app.userInfoReadyCallback = res => {//定义了userInfoReadyCallback等待该函数把值传入
         this.setData({
           userInfo: res.userInfo,
           hasUserInfo: true
